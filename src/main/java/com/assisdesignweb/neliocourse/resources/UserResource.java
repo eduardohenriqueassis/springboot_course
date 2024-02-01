d7743d023cb81233580f9d0aa09dfe8682faf74a
Eduardo Assis <84932877+eduardohenriqueassis@users.noreply.github.com>
2024-02-01 17:59:58 -0300

    Implement User Class

---
+++ src/main/java/com/assisdesignweb/neliocourse/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.assisdesignweb.neliocourse.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.assisdesignweb.neliocourse.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "199123456", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
