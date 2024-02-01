359d63c494bb33161fc56a9ba9171f80c62fe605
Eduardo Assis <84932877+eduardohenriqueassis@users.noreply.github.com>
2024-02-01 17:21:54 -0300

    first commit - adding initial files

---
+++ src/main/java/com/assisdesignweb/neliocourse/NeliocourseApplication.java
@@ -0,0 +1,13 @@
+package com.assisdesignweb.neliocourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeliocourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeliocourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assisdesignweb/neliocourse/NeliocourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assisdesignweb.neliocourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeliocourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
