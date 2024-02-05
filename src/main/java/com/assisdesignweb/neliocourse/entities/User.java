391900cbbedd0821db3330ab03a6ee3f45c7ce3a
Eduardo Assis <84932877+eduardohenriqueassis@users.noreply.github.com>
2024-02-05 19:03:47 -0300

    Adding order class

--- src/main/java/com/assisdesignweb/neliocourse/entities/User.java
+++ src/main/java/com/assisdesignweb/neliocourse/entities/User.java
@@ -1,12 +1,15 @@
 package com.assisdesignweb.neliocourse.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,10 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 
 	public User() {
 	}
@@ -34,6 +41,7 @@ public class User implements Serializable{
 		this.phone = phone;
 		this.password = password;
 	}
+	
 
 	public Long getId() {
 		return id;
@@ -75,6 +83,10 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
